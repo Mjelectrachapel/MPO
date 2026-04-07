ba45fed96c96e3ac8d15083cd7847f7dea2c1155
Mjelectrachapel <mj.ruizredondo1@gmail.com>
2026-04-07 18:42:29 +0200

    Se añaden los ejercicios del 6-10

---
+++ clase 24_03/ejercicio8_cuadrado.py
@@ -0,0 +1,4 @@
+numero= int(input("Introduce un número entero positivo impar\n"))
+for i in range(1,numero+1):
+  print("x"*numero)
+#???????
---
+++ clase 24_03/ejercicio9_sumaacu.py
@@ -0,0 +1,7 @@
+numero= int(input("Introduce un número entero\n"))
+suma=0
+
+while numero !=0:
+  suma= suma + numero
+  numero= int(input("Introduce otro número entero\n"))
+print(f"Suma de todos los números introducidos: {suma}")
