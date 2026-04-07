fd7564b02fe51567c2bab4db633426ed32312242
Mjelectrachapel <mj.ruizredondo1@gmail.com>
2026-04-07 20:09:17 +0200

    cambios en los ejercicios 8 y 10

--- clase 24_03/ejercicio8_cuadrado.py
+++ clase 24_03/ejercicio8_cuadrado.py
@@ -1,4 +1,20 @@
-numero= int(input("Introduce un número entero positivo impar\n"))
-for i in range(1,numero+1):
-  print("x"*numero)
-#???????
+numero = int(input("Introduce un número impar positivo\n"))
+
+print("*"*numero)
+for i in range(1, numero-1):
+
+    fila = "*"
+
+    for j in range(1,numero-1):
+
+        if i == j or numero-1-i == j:
+            fila += "*"
+
+        else:
+            fila += " "
+
+
+    fila += "*"
+    print(fila)
+
+print("*"*numero)
