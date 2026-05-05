bfbd622270ca04eb0851fca4fa5b3328df41e0bd
Mjelectrachapel <mj.ruizredondo1@gmail.com>
2026-05-05 10:12:20 +0200

    terminado el ej 6 de listas

--- clase 21_04/ejercicio6_listas.py
+++ clase 21_04/ejercicio6_listas.py
@@ -3,3 +3,9 @@ Escribe un programa que reciba números hasta la introducción de un 0.
 Por cada número, suponiendo que el 1 representa el lunes, el 2 el martes, etc.
 imprime el nombre del día correspondiente.
 """
+numero = int(input("Introduce un número:" ))
+semana = ["stop","lunes","martes","miércoles","jueves","viernes","sábado","domingo"]
+
+while numero != 0:
+  print(f"El día de la semana es:{semana[numero]} ")
+  numero= int(input("Introduce otro número\n"))
